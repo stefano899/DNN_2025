5aa9f195a8caf68caae8527cd1d19b8db14be742
stefano899 <stefano.rosini@student.univaq.it>
2025-05-22 23:54:02 +0200

    Rifinimento a start.py

--- Architetture/main.py
+++ Architetture/main.py
@@ -6,13 +6,6 @@ from start import start
 
 if __name__ == "__main__":
 
-    # Imposta il seme globalmente PRIMA di qualsiasi operazione casuale
-    seed = 1
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-
     folders = ["Checkpoints", "Plots"]
     decisione = input(
         "Warning: una volta fatto partire il main verranno cancellate le cartelle Checkpoints e Plots (per poi essere ricreate) PROCEDERE? S/n ")
